e5233da0cb826bb8f024c8c20745757b5b81127d
PrasanthRepos <prasanthguntupalli22@gmail.com>
2025-08-10 17:57:45 +0530

    Added a sample database setup functionality

--- app/main.py
+++ app/main.py
@@ -3,13 +3,51 @@ from scalar_fastapi import get_scalar_api_reference
 from typing import Union
 app=FastAPI()
 
+shipments={
+    458:{
+        "weight":.8,
+        "content":"cookware",
+        "status":"placed"
+    },
+    459: {
+    "weight": 1.2,
+    "content": "cutlery",
+    "status": "placed"
+},
+460: {
+    "weight": 0.5,
+    "content": "glassware",
+    "status": "placed"
+},
+461: {
+    "weight": 2.0,
+    "content": "dinner set",
+    "status": "placed"
+},
+462: {
+    "weight": 1.5,
+    "content": "bakeware",
+    "status": "placed"
+}
+
+}
+
+
+@app.get("/shipment/latest")
+def Get_Latest_Shipment():
+    latest_shipment_id=max(shipments.keys())
+
+    return shipments[latest_shipment_id]
+    
 
 @app.get("/shipment/{id}")
 def Get_Shipment(id:int)->dict[str,Union[int,str]]:
+    if id not in shipments:
+        return{
+            "detail":"The given id data does not exist in the database"
+        }
     return {
-        "id":id,
-        "Status":"delivered",
-        "ShipmentId":458
+        shipments[id]
     }
 
 @app.get("/")
